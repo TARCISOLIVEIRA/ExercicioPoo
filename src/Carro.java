bb4074ce279d831e4700aa5bc34158f2e0ce9ddb
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2024-06-28 13:56:39 -0300

    Atualizando

---
+++ src/Carro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tarci
+ */
+public class Carro {
+    
+    public void ligar(){
+        System.out.println("CARRO LIGADO");
+    }
+    
+    public void conferirCombustivel(){
+        System.out.println("Conferindo Combustivel");
+    }
+    
+}
